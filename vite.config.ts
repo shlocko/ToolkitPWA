c1f38abd12ce47a9a03c76c62c1e16dcff8f5627
shlocko <mcdude22@gmail.com>
2022-04-11 18:55:50 -0700

    tried adding icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,36 @@ import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [
       react(),
-      VitePWA(),
+      VitePWA({
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+          manifest: {
+              name: 'Toolkit',
+              short_name: 'Toolkit',
+              description: '',
+              theme_color: '#000000',
+              icons: [
+                  {
+                      src: 'android-chrome-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png',
+                  },
+                  {
+                      src: 'android-chrome-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                  },
+                  {
+                      src: 'android-chrome-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any maskable',
+                  }
+              ]
+          }
+      }),
   ],
     base: "/ToolkitPWA/"
-}
+})
